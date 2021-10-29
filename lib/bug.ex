31b6095a9a93c0345fa80a45c549ad12c929dd8b
Markku Sukanen <markku.sukanen@gmail.com>
2021-10-29 10:52:58 +0300

    encode works... I guess

--- lib/bug.ex
+++ lib/bug.ex
@@ -2,6 +2,7 @@ defmodule Bug do
   @moduledoc """
   Documentation for `Bug`.
   """
+  use Bitwise
 
   @doc """
   Hello world.
@@ -14,5 +15,30 @@ defmodule Bug do
   """
   def hello do
     :world
+  end
+
+  defp base64_set do 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' end
+
+  defp split(num, i) when num > 0 do
+    case num do
+      3 -> for <<x::binary-3 <- i>>, do: x
+      _ -> for <<x::binary-4 <- i>>, do: x
+    end
+  end
+
+  def encode(s) when is_atom(s) do encode Atom.to_string(s) end
+  def encode(s) do
+    [pad, s] = case (rem(byte_size(s), 3)) do
+        1-> ["==", s <> <<0,0>>]
+        2-> ["=", s <> <<0>>]
+        _-> ["", s]
+    end
+    String.slice((split(3, s) |> Enum.map(fn <<a,b,c>> ->
+      n = ((0xff &&& a)<<<16) + ((0xff &&& b) <<<8) + (0xff &&& c)
+      <<Enum.at(base64_set(), (0x3f &&& (n >>> 18))),
+        Enum.at(base64_set(), (0x3f &&& (n >>> 12))),
+        Enum.at(base64_set(), (0x3f &&& (n >>>  6))),
+        Enum.at(base64_set(), (0x3f &&&  n))>>
+    end) |> Enum.join ), 0..-(String.length(pad)+1)) <> pad
   end
 end
--- test/bug_test.exs
+++ test/bug_test.exs
@@ -5,4 +5,8 @@ defmodule BugTest do
   test "greets the world" do
     assert Bug.hello() == :world
   end
+
+  test "hillibillies" do
+    assert Bug.encode("This is a brown fox. What does the fox say? Bip-blibi-bibi-blibibi-bli!") == "VGhpcyBpcyBhIGJyb3duIGZveC4gV2hhdCBkb2VzIHRoZSBmb3ggc2F5PyBCaXAtYmxpYmktYmliaS1ibGliaWJpLWJsaSE="
+  end
 end
