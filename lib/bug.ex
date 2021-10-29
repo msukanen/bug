cde5ac20f2497d3aa80ac683ecb6e2f6110fc208
Markku Sukanen <markku.sukanen@gmail.com>
2021-10-29 12:59:18 +0300

    decode works

--- lib/bug.ex
+++ lib/bug.ex
@@ -18,6 +18,7 @@ defmodule Bug do
   end
 
   defp base64_set do 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' end
+  defp base64_str do "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" end
 
   defp split(num, i) when num > 0 do
     case num do
@@ -26,6 +27,15 @@ defmodule Bug do
     end
   end
 
+  @doc """
+  Base64-encode a string or atom.
+
+  ## Examples
+
+      iex> Bug.encode("This is a string")
+      "VGhpcyBpcyBhIHN0cmluZw=="
+
+  """
   def encode(s) when is_atom(s) do encode Atom.to_string(s) end
   def encode(s) do
     [pad, s] = case (rem(byte_size(s), 3)) do
@@ -40,5 +50,32 @@ defmodule Bug do
         Enum.at(base64_set(), (0x3f &&& (n >>>  6))),
         Enum.at(base64_set(), (0x3f &&&  n))>>
     end) |> Enum.join ), 0..-(String.length(pad)+1)) <> pad
+  end
+
+  @doc """
+  Decode base64 encoded chunk.
+
+  ## Examples
+
+      iex> Bug.decode("VGhpcyBpcyBhIHN0cmluZw==")
+      "This is a string"
+
+  """
+  def decode(s) do
+    if (rem( String.length(s), 4) !== 0) do raise {:error, "Data { s } does not comply with BASE64 encoding spec"} end
+    s = (fn
+      c,_,s when c == "==" -> String.slice(s, 0..-3) <> "AA"
+      _,c,s when c == "=" -> String.slice(s, 0..-2) <> "A"
+      _,_,s -> s
+    end).(String.slice(s, (-2)..(-1)), String.slice(s, -1..-1), s)
+    id = fn x -> {r,_} = :binary.match(base64_str(), List.to_string([x])); r end
+    [h|t] = split(4,s) |> Enum.map(fn <<a,b,c,d>> ->
+        n = (id.(a) <<< 18) + (id.(b) <<< 12) + (id.(c) <<<  6) +  id.(d)
+        <<0xff &&& (n >>> 16), 0xff &&& (n >>>  8), 0xff &&&  n>>
+      end) |> Enum.reverse
+    chp = fn <<x,y,_>> when y == 0 -> <<x>>
+             <<x,y,z>> when z == 0 -> <<x,y>>
+             <<x,y,z>> -> <<x,y,z>> end
+    ((Enum.reverse t) |> Enum.join) <> chp.(h)
   end
 end
--- test/bug_test.exs
+++ test/bug_test.exs
@@ -2,11 +2,13 @@ defmodule BugTest do
   use ExUnit.Case
   doctest Bug
 
-  test "greets the world" do
-    assert Bug.hello() == :world
+  test "encode" do
+    assert Bug.encode("This is a brown fox. What does the fox say? Bip-blibi-bibi-blibibi-bli!") == "VGhpcyBpcyBhIGJyb3duIGZveC4gV2hhdCBkb2VzIHRoZSBmb3ggc2F5PyBCaXAtYmxpYmktYmliaS1ibGliaWJpLWJsaSE="
+    assert Bug.encode("This is a string") == "VGhpcyBpcyBhIHN0cmluZw=="
   end
 
-  test "hillibillies" do
-    assert Bug.encode("This is a brown fox. What does the fox say? Bip-blibi-bibi-blibibi-bli!") == "VGhpcyBpcyBhIGJyb3duIGZveC4gV2hhdCBkb2VzIHRoZSBmb3ggc2F5PyBCaXAtYmxpYmktYmliaS1ibGliaWJpLWJsaSE="
+  test "decode" do
+    assert Bug.decode("VGhpcyBpcyBhIGJyb3duIGZveC4gV2hhdCBkb2VzIHRoZSBmb3ggc2F5PyBCaXAtYmxpYmktYmliaS1ibGliaWJpLWJsaSE=") == "This is a brown fox. What does the fox say? Bip-blibi-bibi-blibibi-bli!"
+    assert Bug.decode("VGhpcyBpcyBhIHN0cmluZw==") == "This is a string"
   end
 end
